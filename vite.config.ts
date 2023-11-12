4a1ad66ccf6eceecb6e7832c9f61f38db48a17cb
Michal Snížek <snizemic@gmail.com>
2023-11-12 10:57:35 +0100

    Fix proxy

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
 				target:
 					'https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt',
 				changeOrigin: true,
+				secure: false,
 				rewrite: path => path.replace(/^\/api/, ''),
 			},
 		},
